667eff48fdbbc791738853f9d52f937260c68478
M.Fadil777 <superdills7@gmail.com>
2025-10-16 19:01:21 +0700

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = "Fadil";
+$age = 15;
+
+
+echo 'Nama : ';
+echo $name;
+echo "\n";
+
+echo 'usia : ';
+echo $age;
+echo "\n";
+
+
+$contoh = "Fadil";
+$$contoh = "Muhammad Fadil";
+
+echo $contoh;
+echo "\n";
+
+echo $Fadil;
+?>
