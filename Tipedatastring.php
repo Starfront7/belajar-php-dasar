1a967e20c88b5fe2eefb1997a8895c328ca524a5
M.Fadil777 <superdills7@gmail.com>
2025-10-16 18:47:26 +0700

    Add files via upload

---
+++ Tipedatastring.php
@@ -0,0 +1,23 @@
+<?php
+
+echo 'Nama : ';
+echo 'Muhammad Fadil';
+echo "\n";
+
+echo "Nama :";
+echo "Muhammad\t Fadil \n ";
+
+
+echo <<<Gimms
+Muhammad Fadil
+sedang belajar PHP
+Gimms;
+
+echo "\n";
+
+echo <<<'Fadil'
+Hallo, nama saya Muhammad Fadil
+Saya kelas X PPLG 1
+Fadil;
+
+?>
